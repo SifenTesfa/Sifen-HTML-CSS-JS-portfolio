6b8424672ac85f85680aef38da361b64dc56c5d6
SifenTesfa <sifentt@gmail.com>
2024-05-22 21:48:17 +0300

    commit

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+ function sendMail(){
+  let parms = {
+    name : document.getElementById("name").value,
+    email : document.getElementById('email').value,
+    subject : document.getElementById('subject').value,
+    message : document.getElementById('message').value
+  }
+  emailjs.send("service_service_7hg70qr","template_template_zfuruws",parms).then(alert("Email Sent!!"))
+ }
   
